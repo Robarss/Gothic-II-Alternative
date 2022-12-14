af0339c77cddb29c5c13de4b5f61abac5fcc6bb3
Robarss <91802191+Robarss@users.noreply.github.com>
2022-12-14 16:06:21 +0100

    Add files via upload

---
+++ scripts/content/AI/Magic/Spells/Spell_ArmyOfDarkness.d
@@ -0,0 +1,48 @@
+
+const int SPL_Cost_ArmyOfDarkness = 150;
+
+instance Spell_ArmyOfDarkness(C_Spell_Proto)
+{
+	time_per_mana = 0;
+	targetCollectAlgo = TARGET_COLLECT_NONE;
+};
+
+
+func int Spell_Logic_ArmyOfDarkness(var int manaInvested)
+{
+	if(Npc_GetActiveSpellIsScroll(self) && (self.attribute[ATR_MANA] >= SPL_COST_SCROLL_ARMYOFDARKNESS))
+	{
+		return SPL_SENDCAST;
+	}
+	else if(self.attribute[ATR_MANA] >= SPL_Cost_ArmyOfDarkness)
+	{
+		return SPL_SENDCAST;
+	}
+	else
+	{
+		return SPL_SENDSTOP;
+	};
+	return FALSE;
+};
+
+func void Spell_Cast_ArmyOfDarkness(var int spellLevel)
+{
+	if(Npc_GetActiveSpellIsScroll(self))
+	{
+		self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - SPL_COST_SCROLL_ARMYOFDARKNESS;
+	}
+	else
+	{
+		self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - SPL_Cost_ArmyOfDarkness;
+	};
+	if(Npc_IsPlayer(self))
+	{
+		Wld_SpawnNpcRange(self,Summoned_Skeleton,6,800);
+	}
+	else
+	{
+		Wld_SpawnNpcRange(self,Skeleton,6,800);
+	};
+	self.aivar[AIV_SelectSpell] += 1;
+};
+
---
+++ scripts/content/AI/Magic/Spells/Spell_SummonDemon.d
@@ -0,0 +1,48 @@
+
+const int SPL_Cost_SummonDemon = 120;
+
+instance Spell_SummonDemon(C_Spell_Proto)
+{
+	time_per_mana = 0;
+	targetCollectAlgo = TARGET_COLLECT_NONE;
+};
+
+
+func int Spell_Logic_SummonDemon(var int manaInvested)
+{
+	if(Npc_GetActiveSpellIsScroll(self) && (self.attribute[ATR_MANA] >= SPL_COST_SCROLL_SUMMONDEMON))
+	{
+		return SPL_SENDCAST;
+	}
+	else if(self.attribute[ATR_MANA] >= SPL_Cost_SummonDemon)
+	{
+		return SPL_SENDCAST;
+	}
+	else
+	{
+		return SPL_SENDSTOP;
+	};
+	return FALSE;
+};
+
+func void Spell_Cast_SummonDemon()
+{
+	if(Npc_GetActiveSpellIsScroll(self))
+	{
+		self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - SPL_COST_SCROLL_SUMMONDEMON;
+	}
+	else
+	{
+		self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - SPL_Cost_SummonDemon;
+	};
+	if(Npc_IsPlayer(self))
+	{
+		Wld_SpawnNpcRange(self,Summoned_Demon,1,1000);
+	}
+	else
+	{
+		Wld_SpawnNpcRange(self,Demon,1,1000);
+	};
+	self.aivar[AIV_SelectSpell] += 1;
+};
+
